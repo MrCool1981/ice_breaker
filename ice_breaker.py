5730bdb04430f3921c08104e75eadbcc70c89f0f
Alex N <cool.novikov@gmail.com>
2024-07-24 16:49:32 +1200

    updates

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_openai import AzureChatOpenAI
 from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
@@ -20,11 +20,11 @@ if __name__ == "__main__":
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = AzureChatOpenAI(azure_deployment="gpt-4")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+        linkedin_profile_url="https://www.linkedin.com/in/alexthepro/", mock=True
     )
     res = chain.invoke(input={"information": linkedin_data})
 
